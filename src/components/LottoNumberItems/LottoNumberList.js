7ebbf48c41b267c429d761ca48dbf6d65ceb7268
Jinhyeok <44572131+KJH9612@users.noreply.github.com>
2022-05-09 23:14:59 +0900

    Lotto Component 구현
    * Lotto Number Component

---
+++ src/components/LottoNumberItems/LottoNumberList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import LottoNumberItem from './../LottoNumberItem/LottoNumberItem';
+
+
+export default function LottoNumberList({reset}) {
+  const lottoMaxCount = 6
+  let lottoData = Array()
+  const lottoDataCheck = ({LottoNumber, color}) => {
+    if(color === 'success') {
+      lottoData = lottoData.filter(item => {
+        return item !== LottoNumber
+      })
+    }
+    else{
+      if(lottoData.length >= lottoMaxCount){
+        alert('최대 6개까지 선택 가능합니다.')
+        return false
+      }
+      lottoData.push(LottoNumber)
+    }
+    return true
+  }
+
+  const LottoItems = Array.from(
+      new Array(45),
+      (x, i) => (<LottoNumberItem key={i + 1} LottoNumber={i + 1} lottoData={lottoDataCheck}/>)
+  )
+  return (
+    <>
+      { LottoItems }
+    </>
+  )
+}
