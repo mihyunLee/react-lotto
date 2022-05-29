b79deac35adfabb990749e6fe7d6cb2e311dddbb
mihyunLee <mihy621@gmail.com>
2022-05-29 23:16:25 +0900

    docs: 기존 프로젝트 파일 삭제

--- src/backup/Modals/ModalsContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const ModalsDispatchContext = createContext({
-  // 필요한 함수
-  open: () => {},
-  close: () => {},
-});
-
-export const ModalsStateContext = createContext([]);
